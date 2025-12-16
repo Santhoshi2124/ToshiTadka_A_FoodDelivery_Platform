84b3d0d2591f600027d6b572ebfc940c399edce9
Santhoshi2124 <santoshimk2004@gmail.com>
2025-12-16 19:36:20 +0530

    Fix base path in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<ToshiTadka_A_FoodDelivery_Platform>/', 
+  base: '/ToshiTadka_A_FoodDelivery_Platform/', 
 })
